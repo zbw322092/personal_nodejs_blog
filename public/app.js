c289e981a90d4528b57bd91bf2d57d3d7746fd86
zbw322092 <bz4e14@soton.ac.uk>
2016-11-20 23:16:25 +0800

    initial sign up view

--- public/app.js
+++ public/app.js
@@ -2,13 +2,13 @@ var app = angular.module('personal_blog_app', ['ngRoute']);
 
 app.config(function($routeProvider) {
 	$routeProvider
-		.when('/', {
+		.when('/aaa', {
 			controller: 'blogCtrl',
 			templateUrl: 'public/views/index.html'
 		})
-		// .when('/sign_up', {
-		// 	controller: 'blogCtrl',
-		// 	templateUrl: 'public/views/sign_up.html'
-		// })
-		.otherwise({redirectTo: '/'});
+		.when('/sign_up', {
+			controller: 'signUpCtrl',
+			templateUrl: 'public/views/sign_up.html'
+		})
+		.otherwise({redirectTo: '/aaa'});
 });
---
+++ public/controllers/signUpCtrl.js
@@ -0,0 +1,3 @@
+app.controller('signUpCtrl', function($scope) {
+	
+});
